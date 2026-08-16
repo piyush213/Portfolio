a4c151bb9cdda1e9484c14d8b340fb812c51d829
piyushdungrani <piyush.espsofttech@gmail.com>
2026-08-16 23:53:20 +0530

    latest code update

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,18 +1,56 @@
 import { ArrowUpRight, Play } from "lucide-react";
+import {
+    FaReact,
+    FaJs,
+    FaHtml5,
+    FaCss3Alt,
+    FaBootstrap,
+    FaGitAlt,
+} from "react-icons/fa";
+
+import {
+    SiNextdotjs,
+    SiTypescript,
+    SiTailwindcss,
+} from "react-icons/si";
+
 
 function Home() {
 
     const skills = [
-        "React.js",
-        "Next.js",
-        "JavaScript",
-        "TypeScript",
-        "HTML5",
-        "CSS3",
-        "Tailwind CSS",
-        "Bootstrap",
-        "Git",
-        "REST API",
+        { name: "React.js", icon: <FaReact fill="#09e5f4" size={40} />, color: "#09e5f4" },
+        { name: "Next.js", icon: <SiNextdotjs fill="black" size={40} />, color: "#2c3951" },
+        { name: "JavaScript", icon: <FaJs fill="#f4d02a" size={40} />, color: "#f4d02a" },
+        { name: "TypeScript", icon: <SiTypescript fill="#0773ce" size={40} />, color: "#0773ce" },
+        { name: "HTML5", icon: <FaHtml5 fill="#eb5523" size={40} />, color: "#eb5523" },
+        { name: "CSS3", icon: <FaCss3Alt fill="#eb5523" size={40} />, color: "#eb5523" },
+        { name: "Tailwind CSS", icon: <SiTailwindcss fill="#1ad0e8" size={40} />, color: "#1ad0e8" },
+        { name: "Bootstrap", icon: <FaBootstrap fill="#7d54fc" size={40} />, color: "#7d54fc" },
+        { name: "Git", icon: <FaGitAlt fill="#fa4529" size={40} />, color: "#fa4529" },
+        { name: "REST API", icon: "🔗", color: "#16c0ad" },
+    ];
+    const projects = [
+        {
+            title: "ArtFungible",
+            image: "/images/project/artfungible.png",
+            description: "NFT Marketplace Platform",
+            tech: ["React", "Web3", "Bootstrap"],
+            live: "https://artfungible.io/",
+        },
+        {
+            title: "Property Nu",
+            image: "/images/project/propertynu.png",
+            description: "Real Estate Platform",
+            tech: ["React", "Tailwind"],
+            live: "https://www.propertynu.com/",
+        },
+        {
+            title: "Purple Swap",
+            image: "/images/project/purpleswap.png",
+            description: "Crypto Exchange Platform",
+            tech: ["React", "Web3"],
+            live: "https://purpleswap.io/",
+        },
     ];
     const experiences = [
         {
@@ -38,6 +76,25 @@ function Home() {
             description:
                 "Designed and developed websites with a strong focus on usability, responsiveness and clean UI.",
             progress: "76%",
+        },
+    ];
+
+    const counters = [
+        {
+            number: "7+",
+            label: "Years Experience",
+        },
+        {
+            number: "100+",
+            label: "Projects Completed",
+        },
+        {
+            number: "50+",
+            label: "Happy Clients",
+        },
+        {
+            number: "10+",
+            label: "Technologies",
         },
     ];
 
@@ -85,7 +142,7 @@ function Home() {
             <header className="fixed top-0 z-50 w-full backdrop-blur border-b border-slate-800">
                 <div className="max-w-6xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
                     <div className="brand">
-                        <img src="/images/p.png" width={`50px`} />
+                        <img src="/images/logo/p.png" width={`60px`} />
                     </div>
 
                     <nav className="hidden md:flex gap-8 text-sm lg:text-base">
@@ -143,12 +200,12 @@ function Home() {
                 id="about"
                 className=" px-6 py-20 bg-[#0B0D0E]"
             >
-                <div className="about-area about-area-primery max-w-6xl mx-auto">
+                <div className="about-area about-area-primery">
 
-                    <div className="grid md:grid-cols-2 gap-12 items-center">
+                    <div className="grid md:grid-cols-2 gap-12 items-center max-w-6xl mx-auto">
                         <div className="about-img">
                             <div className="about-shape">
-                                <div className="about-img-section text-center h-[480px] w-[480px] bg-cover bg-center bg-no-repeat"
+                                <div className="about-img-section text-center h-[480px] w-[420px] bg-cover bg-center bg-no-repeat"
                                     style={{
                                         backgroundImage:
                                             'url("https://wp.quomodosoft.com/glint/wp-content/uploads/2019/12/about1-1.png")',
@@ -183,21 +240,117 @@ function Home() {
                 </h2>
 
                 <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
-                    {skills.map((skill) => (
+                    {skills.map((skill, index) => (
                         <div
-                            key={skill}
-                            className="bg-slate-900 rounded-lg p-5 text-center hover:bg-cyan-500 transition"
+                            key={index}
+                            className="rounded-xl p-5 text-center transition-all duration-300 hover:-translate-y-2 hover:scale-105"
+                            style={{
+                                background: "#11192a",
+                                borderTop: `3px solid ${skill.color}`,
+                                boxShadow: `
+      10px 10px 20px rgba(0,0,0,0.6),
+      -5px -5px 12px rgba(255,255,255,0.04)
+    `,
+                            }}
                         >
-                            {skill}
+                            <div
+                                className="flex justify-center mb-4"
+                            >
+                                {skill.icon}
+                            </div>
+
+                            <h3 className="text-white font-medium">{skill.name}</h3>
                         </div>
                     ))}
+
+                </div>
+            </section>
+            <section id="counter" className="py-20">
+                <div className="max-w-6xl mx-auto px-6">
+                     <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+                    {counters.map((counter, index) => (
+                        <div
+                            key={index}
+                            className="bg-[#11192a] rounded-xl p-8 text-center"
+                            style={{
+                                borderTop: `3px solid ${counter.color}`,
+                                boxShadow: `0 0 20px ${counter.color}20`,
+                            }}
+                        >
+                            <h3
+                                className="text-3xl font-bold"
+                                style={{ color: counter.color }}
+                            >
+                                {counter.number}
+                            </h3>
+
+                            <p className="mt-2 text-gray-300">
+                                {counter.label}
+                            </p>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+
+            </section>
+            {/* Project */}
+            <section id="project" className="py-20">
+                <div className="max-w-6xl mx-auto px-6">
+                    <h2 className="text-3xl font-bold text-center mb-12">
+                        My Projects
+                    </h2>
+
+                    <div className="grid md:grid-cols-3 gap-8">
+                        {projects.map((project, index) => (
+                            <div
+                                key={index}
+                                className="bg-[#11192a] rounded-xl overflow-hidden
+        transition-all duration-300 hover:-translate-y-2"
+                            >
+                                <img
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="w-full h-40 object-contain"
+                                />
+
+                                <div className="p-5">
+                                    <h3 className="text-2xl font-semibold mb-2">
+                                        {project.title}
+                                    </h3>
+
+                                    <p className="text-gray-400 mb-4">
+                                        {project.description}
+                                    </p>
+
+                                    <div className="flex flex-wrap gap-2 mb-4">
+                                        {project.tech.map((tech) => (
+                                            <span
+                                                key={tech}
+                                                className="px-3 py-1 bg-cyan-500/20 rounded-full text-cyan-400 text-sm"
+                                            >
+                                                {tech}
+                                            </span>
+                                        ))}
+                                    </div>
+
+                                    <a
+                                        href={project.live}
+                                        target="_blank"
+                                        className="inline-block bg-cyan-500 px-3 py-1 rounded-lg text-sm"
+                                    >
+                                        Live Demo
+                                    </a>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
 
             {/* Contact */}
             <section
                 id="contact"
-                className="max-w-6xl mx-auto px-6 py-20 text-center"
+                className="max-w-6xl mx-auto px-6 py-20 text-center bg-[url('images/map-1.png')] bg-cover bg-center bg-no-repeat"
             >
                 <h2 className="text-4xl font-bold mb-5">
                     Let's Work Together
